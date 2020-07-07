2176dce6b5acf2a38e847205ceebbc7962e9d8af
krishna6pulipati <krishna6.pulipati@gmail.com>
2020-07-07 16:35:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Changing the page
         </a>
       </header>
     </div>
